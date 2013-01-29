ed51cce3d1e5e6f543838b0b9bac807b3c7adee7
Baudouin Feildel <b.feildel@gmail.com>
2013-01-29 16:28:08 +0100

    Test commande AT et vidéo

--- sources/CommandeAT/send.php
+++ sources/CommandeAT/send.php
@@ -5,14 +5,14 @@
 
 $host = "192.168.1.1";
 $port = "5556";
-$ident = 6;
+$ident = 0;
 $tmpAT;
 
 $stdin = fopen("php://stdin", "r");
 
-echo "Ouverture de la socket";
-$socket = fsockopen("udp://" .$host, $port);
-if(!$socket)
+echo "Ouverture de la socket\n";
+$socket_at = fsockopen("udp://" .$host, $port);
+if(!$socket_at)
 {
 	echo "Erreur d'ouverture de la Socket\n";
 	echo "Goodbye\n";
@@ -22,22 +22,36 @@ else
 	echo "Test d'envoi de donnée\n";
 
 	echo "Configuration du drone\n";
-	$tmpAT = "AT*CONFIG=" . ++$ident . ",\"control:altitude_max\",\"2000\"";
+	$tmpAT = "AT*CONFIG=" . ++$ident . ",\"control:altitude_max\",\"2000\"\r";
 	echo "Commande AT: " . $tmpAT . "\n";
-	fwrite($socket, $tmpAT);
+	fwrite($socket_at, $tmpAT);
+	echo "Configuration du drone\n";
+	$tmpAT = "AT*FTRIM=" . ++$ident . ",\r";
+	echo "Commande AT: " . $tmpAT . "\n";
+	fwrite($socket_at, $tmpAT);
 
 	echo "Décollage !!!\n";
 	echo "Commande AT: ";
 	$tmpAT = "AT*REF=" . ++$ident .",290718208\r";
 	echo $tmpAT . "\n";
-	fwrite($socket, $tmpAT);
+	fwrite($socket_at, $tmpAT);
+
+	for($i = 0 ; $i < 100 ; $i++)
+	{
+	  usleep(100000);
+
+	  echo "Vol stationnaire\n";
+	  $tmpAT = "AT*PCMD=" . ++$ident . ",1,0,0,0,0\r";
+	  echo "Commande AT: " . $tmpAT . "\n";
+	  fwrite($socket_at, $tmpAT);
+	}
 
 	fgetc($stdin);
 
 	echo "Atterissage...\n";
-	$tmpAT = "AT*REF=" . ++$ident .",290718208\r";
+	$tmpAT = "AT*REF=" . ++$ident .",290717696\r";
 	echo "Commande AT: " . $tmpAT . "\n";
-	fwrite($socket, $tmpAT);
+	fwrite($socket_at, $tmpAT);
 }
 
 ?>
