9186d17d22125159fd2983c0aa9060ad62f1cf52
Thibaut Marty <thibautdotmarty@gmail.com>
2013-01-29 16:24:42 +0100

    fichier avec connexion WiFi+ envoi de paquets UDP

--- sources/wifi/connexion.c
+++ sources/wifi/connexion.c
@@ -1,3 +1,40 @@
+/**
+	\defgroup Connexion
+	\defgroup UDP
+	\defgroup IO
+**/
+
+
+/*
+ * Define the number of socket buffers that will be allocated for
+ * UDP sockets.  We only need one UDP socket, so one socket buffer
+ * will be enough.
+ */
+#define MAX_UDP_SOCKET_BUFFERS 1
+
+/*
+ * UDP demo configuration
+ */
+
+/* How often (in seconds) to send out UDP packet. */
+#define  HEARTBEAT_RATE 	10
+
+/* what local UDP port to use - packets we send come from here */
+#define LOCAL_PORT	5556
+
+/*
+ * Where to send the heartbeats. If it is set to all '255's,
+ * it will be a broadcast packet.
+ */
+//#define REMOTE_IP			"192.168.1.1"
+#define REMOTE_IP			"255.255.255.255" /*broadcast*/
+
+/* the destination port to send to */
+#define REMOTE_PORT	LOCAL_PORT
+
+/* ******************************** */
+
+
 /*
  * NETWORK CONFIGURATION
  * Please see the function help (Ctrl-H) on TCPCONFIG for instructions on
@@ -5,11 +42,16 @@
  */
 
 #define TCPCONFIG 5
-//#define MODE_DEBUG 1
 
+#define MODE_DEBUG 1
+
+
+/** \typedef connect_status Permet de définir le statut de la connexion en cours
+	\ingroup Connexion
+**/
 typedef enum
 {
-  INOCCUPE,
+	INOCCUPE,
 	RECHERCHE,
 	TROUVE,
 	CONNECTE
@@ -21,37 +63,85 @@ typedef enum
 #use "rcm56xxw.lib"
 
 #memmap xmem
-
+/**	\var connect variable globale qui défini le statut de la connexion en cours
+	\ingroup Connexion
+**/
 connect_status connect = INOCCUPE;
 
+
+/**	\fn void ledDS1(char state)
+	\ingroup IO
+	\brief change l'état de la LED DS1
+	\param state 1 : allumé ; 0 : éteinte
+	\author Thibaut Marty
+**/
 void ledDS1(char state)
 {
 	BitWrPortI(PDDR, &PDDRShadow, !state, 0/*ds1*/);
 }
 
+/**	\fn int send_packet(char* str, const char far * ip, word port, udp_Socket *sock)
+	\ingroup UDP
+	\brief envoie une chaîne de caractère
+	\param str chaîne à envoyé
+	\param ip ip destination
+	\param port port de l'envoi
+	\param sock pointeur sur udp_Socket utilisée*
+	\return 0 : paquet envoyé ; -1 : erreur mais la socket est ouverte ; -2 : erreur, la socket est ouverte
+	\author Thibaut Marty
+**/
+int send_packet(char* str, const char far * ip, word port, udp_Socket *sock)
+{	
+	// envoi le paquet
+	if(udp_send(sock, str, strlen(str) + 1) < 0) // s'il y a une erreur
+	{
+#ifdef MODE_DEBUG
+		printf("Erreur d'envoi du paquet :\n;;;%s\n", str);
+#endif
+		sock_close(sock);	// ferme la socket puis la re-ouvre
+		if(!udp_open(sock, port, resolve(ip), port, NULL))
+		{
+#ifdef MODE_DEBUG
+			printf("Erreur de reouverture de la socket\n");
+#endif
+			return -1;
+		}
+		else
+		{
+#ifdef MODE_DEBUG
+			printf("Erreur de reouverture de la socket\n");
+#endif
+			return -2;
+		}
+	}
+#ifdef MODE_DEBUG
+   else
+		printf("Paquet envoye :\n;;;%s\n", str);
+#endif
 
+	tcp_tick(NULL);
+	return 0;
+}
 
-/****************************************************************************
-	rxsignal_cmp
-
-	qsort comparison, based on rx signal strength.
-
-   Inputs:
-      a, b  -- far pointers to _wifi_wln_scan_bss, a struct populated by
-               the WIFI SCAN, including rx_signal (relative receive signal
-               strength).
-
-	Return value: > 0 if a > b
-					  < 0 if a < b
-					  0   if a == b
-
-****************************************************************************/
+/**	\fn int rxsignal_cmp(far _wifi_wln_scan_bss *a, far _wifi_wln_scan_bss *b)
+	\ingroup Connexion
+	\brief fonction qui permet le trie les points d'accès WiFi par puissance du signal
+	\param a un _wifi_wln_scan_bss qui correspond à un point d'accès
+	\param b un _wifi_wln_scan_bss qui correspond à un point d'accès
+	\author Dynamic C
+**/
 int rxsignal_cmp(far _wifi_wln_scan_bss *a, far _wifi_wln_scan_bss *b)
 {
 	return b->rx_signal - a->rx_signal;
 }
 
-
+/**	\fn root void scan_assoc_callback(far wifi_scan_data* data)
+	\ingroup Connexion
+	\brief fonction de callback qui traite les points d'accès donnés :
+		recherche le point d'accès du drone et se connecte
+	\param data liste des points d'accès
+	\author Thibaut Marty
+**/
 root void scan_assoc_callback(far wifi_scan_data* data)
 {
 	far _wifi_wln_scan_bss *bss;
@@ -60,13 +150,13 @@ root void scan_assoc_callback(far wifi_scan_data* data)
 
 	connect = RECHERCHE;
 	
-	bss = data->bss;	// pointeur sur les points d'accÃ¨s disponibles
+	bss = data->bss;	// pointeur sur les points d'accès disponibles
 	
 	_f_qsort(bss, data->count, sizeof(bss[0]), rxsignal_cmp);	// trie en fonction de la puissance du signal
 #ifdef MODE_DEBUG
 	printf("\nResultat du scan des points d'acces :\n");
 #endif
-	for(i = 0; i < data->count; i++)	// affiche les points d'accÃ¨s disponibles
+	for(i = 0; i < data->count; i++)	// affiche les points d'accès disponibles
 	{
 		wifi_ssid_to_str(ssid_str, bss[i].ssid, bss[i].ssid_len);
 #ifdef MODE_DEBUG
@@ -79,23 +169,21 @@ root void scan_assoc_callback(far wifi_scan_data* data)
 #endif
 	for(i = 0; i < data->count && connect != TROUVE; i++)
 	{
-		wifi_ssid_to_str(ssid_str, bss[i].ssid, bss[i].ssid_len);	// donne une chaÃ®ne de caractÃ¨re imprimable
+		wifi_ssid_to_str(ssid_str, bss[i].ssid, bss[i].ssid_len);	// donne une chaîne de caractère imprimable
 		
 #ifdef MODE_DEBUG
 		printf("[%2d] comparaison de '%s' avec 'ardrone2_xxxxxx' : ", i, ssid_str);
 #endif
 		
-		ctemp = ssid_str[9];	// sauvegarde du caractÃ¨re
-		ssid_str[9] = '\0';		// tronque Ã  9 caractÃ¨res (ardrone2_)
-		
-		while(!tcp_tick(NULL));	// attend que tout ce qu'il faut soit fait
+		ctemp = ssid_str[9];	// sauvegarde du caractère
+		ssid_str[9] = '\0';		// tronque à 9 caractères (ardrone2_)
 		
 		if(!strcmp("ardrone2_", ssid_str))	// si le ssid correspond
 		{
 #ifdef MODE_DEBUG
 			printf("oui\n");
 #endif
-			ssid_str[9] = ctemp;	// remet du caractÃ¨re enlevÃ©
+			ssid_str[9] = ctemp;	// remet du caractère enlevé
 			
 			// Need near copy of SSID to call ifconfig.  ssid will be promoted to
 			// far for this call, but the results will be in ssid as a near
@@ -109,7 +197,7 @@ root void scan_assoc_callback(far wifi_scan_data* data)
 #ifdef MODE_DEBUG
 				printf(" => demande de connexion a %s...\n", ssid);
 #endif
-				connect = TROUVE;	// indique que le drone est trouvÃ©
+				connect = TROUVE;	// indique que le drone est trouvé
 				// puis il faut attendre que la connexion soit effective (dans connexion())
 			}
 #ifdef MODE_DEBUG
@@ -131,6 +219,11 @@ root void scan_assoc_callback(far wifi_scan_data* data)
 #endif
 }
 
+/**	\fn void connexion(void)
+	\ingroup Connexion
+	\brief fonction qui se connecte au drone. Elle se quitte uniquement si la connexion est effective
+	\author Thibaut Marty
+**/
 void connexion(void)
 {
 	wifi_status status;
@@ -150,10 +243,10 @@ void connexion(void)
 		connect_timeout = 1;	// reset
 		tcp_tick(NULL);
 		ifdown(IF_WIFI0);
-		while(ifpending(IF_WIFI0) != IF_DOWN)	// attend que l'Ã©ventuelle ancienne connexion soit arrÃªtÃ©e
+		while(ifpending(IF_WIFI0) != IF_DOWN)	// attend que l'éventuelle ancienne connexion soit arrêtée
 		   tcp_tick(NULL);
 
-		// Donne la fonction de callback avant la requÃªte de scan
+		// Donne la fonction de callback avant la requête de scan
 		ifconfig(IF_WIFI0, IFS_WIFI_SCAN, scan_assoc_callback, IFS_END);
 		do
 		{
@@ -164,10 +257,10 @@ void connexion(void)
 			}
 			costate
 			{
-				connect_timeout++; // jusqu'Ã  l'overflow => sort de la boucle
+				connect_timeout++; // jusqu'à l'overflow => sort de la boucle
 				waitfor(DelayMs(20)); // 5,10 secondes de tentatives de connexion
 			}
-		} while(connect != TROUVE && connect_timeout); // attend que le drone soit trouvÃ©
+		} while(connect != TROUVE && connect_timeout); // attend que le drone soit trouvé
 		
 		
 		if(connect == TROUVE)	// (pas de timeout)
@@ -182,13 +275,13 @@ void connexion(void)
 			{
 				costate
 				{
-					ifconfig(IF_WIFI0, IFG_WIFI_STATUS, &status, IFS_END);	// rÃ©cupere le statut
+					ifconfig(IF_WIFI0, IFG_WIFI_STATUS, &status, IFS_END);	// récupere le statut
 					while(!tcp_tick(NULL));
 					waitfor(DelayMs(1));
 				}
 				costate
 				{
-					connect_timeout++; // jusqu'Ã  l'overflow => sort de la boucle
+					connect_timeout++; // jusqu'à l'overflow => sort de la boucle
 					waitfor(DelayMs(20)); // 5,10 secondes de tentatives de connexion
 				}
 			} while(status.state != WLN_ST_ASSOC_ESS && connect_timeout); // attend que la connexion soit effective
@@ -217,6 +310,9 @@ void connexion(void)
 
 void main(void)
 {
+	udp_Socket sock;
+	char buff[1025];
+	
 	brdInit();
 	ledDS1(0);
 #ifdef MODE_DEBUG
@@ -227,15 +323,33 @@ void main(void)
 	printf("\n\nCONNECTE\n\n");
 #endif
 	ledDS1(1);
+	
+
+	
+	strcpy(buff, "AT*CONFIG=1,\"control:altitude_max\",\"2000\"\r");
+	
+	
+	if(!udp_open(&sock, LOCAL_PORT, resolve(REMOTE_IP), REMOTE_PORT, NULL))
+	{
+#ifdef MODE_DEBUG
+		printf("udp_open failed!\n");
+#endif
+	}
+	for(;;)
+	{
+		tcp_tick(NULL);
+		costate
+		{
+			waitfor(IntervalSec(HEARTBEAT_RATE));
+			send_packet(buff, REMOTE_IP, LOCAL_PORT, &sock);
+		}
+	}
+	
+	
+	/*sprintf(buf, "AT*CONFIG=%d,\"control:altitude_max\",\"2000\"\r", ++ident);
+	sprintf(buf, "AT*REF=%d,290718208\r", ++ident);
+	sprintf(buf, "AT*REF=%d,290717696\r", ++ident);*/
+	
 	for(;;);
-	/*{
-		ifconfig(IF_WIFI0, IFG_WIFI_STATUS, &status, IFS_END);
-		while(!tcp_tick(NULL));	// attend que tout ce qu'il faut soit fait
-		print_status(&status);
-		printf(" => affichage du statut mac > SSID : %*s", _wifi_macStatus.ssid_len, _wifi_macStatus.ssid);
-		while(!kbhit())
-			tcp_tick(NULL);
-		getchar();
-	}*/
 }
 
